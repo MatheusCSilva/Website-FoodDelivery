4b92a4bffb597e7e2a08d59e29c8a10d186880aa
Matheus <jinkamuya@gmail.com>
2022-12-29 12:02:50 -0300

    ninety-seven percent of the project completed

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if (window.scrollY > 60) {
+        document.querySelector('#scroll-top').classList.add('active')
+    } else {
+        document.querySelector('#scroll-top').classList.add('active')
+    }
+}
